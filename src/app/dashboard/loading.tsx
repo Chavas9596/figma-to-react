db3e1b3f04e2ef173940857119746eec6e6bbff1
Chavas9596 <luckyangel950906@gmail.com>
2024-12-27 13:43:38 -0500

    feat: Call POST API.

--- src/app/dashboard/loading.tsx
+++ src/app/dashboard/loading.tsx
@@ -6,8 +6,9 @@ const SkeletonLoading = () => {
           {[...Array(8)].map((_, index) => (
             <div key={index} className="border border-blue-300 shadow rounded-md p-4 max-w-sm w-full mx-auto">
             <div className="animate-pulse flex space-x-4">
+              <div className="rounded-full bg-slate-700 h-10 w-10"></div>
               <div className="flex-1 space-y-6 py-1">
-                <div className="h-48 bg-slate-700 rounded"></div>
+                <div className="h-2 bg-slate-700 rounded"></div>
                 <div className="space-y-3">
                   <div className="grid grid-cols-3 gap-4">
                     <div className="h-2 bg-slate-700 rounded col-span-2"></div>
